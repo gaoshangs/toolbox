e07f25238c738d335cb1e7fef18fab47e83f8d9b
gaoshangwang <i_can2013@163.com>
2018-08-30 19:09:23 +0800

    update jsclipper

--- php/jsclipper/check_self_intersection.php
+++ php/jsclipper/check_self_intersection.php
@@ -11,16 +11,18 @@ $data = json_decode($request['data'], true);
 $dbh = new PDO('mysql:host=localhost;dbname=crm_db', 'root', 'root');
 
 foreach ($data as $id => $len) {
-    $ret = add($dbh, $id, $len);
-    file_put_contents('./run.log', $id.','.$len.','.$ret."\n", FILE_APPEND);
+    update($dbh, $id, $len);
+    file_put_contents('./run.log', $id.','.$len."\n", FILE_APPEND);
 }
+echo 'success';
 
-function add(PDO $dbh, $area_id, $polygon_num) {
-    $sth = $dbh->prepare('INSERT INTO simply_sale_area(sale_area_id, polygon_num) VALUES(:sale_area_id, :polygon_num)');
+function update(PDO $dbh, $area_id, $polygon_num) {
+    $sth = $dbh->prepare('update t_sale_area_base set is_intersect = :is_intersect, intersect_num = :polygon_num, is_processed = 1 where id = :sale_area_id');
     $sth->execute(
         [
             ':sale_area_id' => $area_id,
-            ':polygon_num' => $polygon_num
+            ':polygon_num' => $polygon_num - 1,
+            ':is_intersect' => $polygon_num > 1 ? 1 : 0,
         ]
     );
     return $sth->rowCount();
